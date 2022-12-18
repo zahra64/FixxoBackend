363c098c82cb1ba17c3b33d10e70fb4a66e3287a
Zahra Parhizkari <zarapar64@gmail.com>
2022-12-18 22:11:43 +0100

    used MenuIcon typescript

--- src/components/MenuIcon.tsx
+++ src/components/MenuIcon.tsx
@@ -3,21 +3,23 @@ import { NavLink } from 'react-router-dom'
 
 
 export interface IMenuIconType{
-  link: string
-  icon: string
-  quantity: string
-  hideOnMobile: boolean
+  link?: any
+  icon?: string
+  quantity?: string
+  hideOnMobile?: boolean
 }
+
+
 
 const MenuIcon: React.FC<IMenuIconType> = ({link, icon, quantity, hideOnMobile}) => {
   return (
-    // <NavLink className="menu-icon" to={link} end>
+
    
-   <NavLink className={`menu-icon ${hideOnMobile ? "d-none d-md-flex" : ""}`}  to={link} end>
-      <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme">{quantity}</span>
-      <i className={icon}></i>
-    </NavLink>
-  )
+    <NavLink className={`menu-icon ${{hideOnMobile} ? "d-none d-md-flex" : ""}`}  to={link} end>
+    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme">{quantity}</span>
+    <i className={icon}></i>
+  </NavLink>
+)
 }
 
 export default MenuIcon
--- src/sections/MainMenuSection.tsx
+++ src/sections/MainMenuSection.tsx
@@ -2,12 +2,7 @@ import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import MenuIcon from '../components/MenuIcon'
 
-export interface IMenuIconType{
-  link: string
-  icon: string
-  quantity: string
-  hideOnMobile: boolean
-}
+
 
 const MainMenuSection: React.FC = () => {
   const [showMenu, setShowMenu] = useState<boolean>(false)
@@ -28,28 +23,42 @@ const MainMenuSection: React.FC = () => {
             <NavLink className="menu-link" to="/contacts" end>Contacs</NavLink>
         </div>
         <div className="menu-icons">
-          <NavLink to="/search" end className="menu-icon">
-            <i className="fa-regular fa-magnifying-glass"></i>
-          </NavLink>
-          <NavLink to="/compare" end className="menu-icon">
-            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme"></span>
-            <i className="fa-regular fa-code-compare"></i>
-          </NavLink>
-          <NavLink to="/wishlist" end className="menu-icon">
-            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme"></span>
-            <i className="fa-regular fa-heart"></i>
-          </NavLink>
-          <button className="menu-icon" type="button" data-bs-toggle="offcanvas" data-bs-target="#shoppingCart" aria-controls="show">
-            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme"></span>
-            {/* <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme">{totalQuantity}</span> */}
-            <i className="fa-regular fa-bag-shopping"></i>
-          </button>
-          <button onClick={toggleMenu} className="d-xl-none menu-icon btn-menu-icon">
-            <i className="fa-regular fa-bars"></i>
-          </button>
+            <MenuIcon link="/search" icon="fa-regular fa-magnifying-glass" />
+            <MenuIcon hideOnMobile={true} link="/compare" icon="fa-regular fa-code-compare" />
+            <MenuIcon hideOnMobile={true} quantity="3" link="/wishlist" icon="fa-regular fa-heart" />
+            <MenuIcon quantity="4" link="/shoppingcart" icon="fa-regular fa-bag-shopping" />
+
+            {/* <button className="d-xl-none menu-icon btn-menu-icon"><i className="fa-regular fa-bars"></i></button> */}
+            <button onClick={toggleMenu} className="d-xl-none menu-icon btn-menu-icon"><i className="fa-regular fa-bars"></i></button>
         </div>
 </nav>
   )
 }
 
 export default MainMenuSection
+
+
+
+// {/* <div className="menu-icons">
+//           <NavLink to="/search" end className="menu-icon">
+//             <i className="fa-regular fa-magnifying-glass"></i>
+//           </NavLink>
+//           <NavLink to="/compare" end className="menu-icon">
+//             <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme"></span>
+//             <i className="fa-regular fa-code-compare"></i>
+//           </NavLink>
+//           <NavLink to="/wishlist" end className="menu-icon">
+//             <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme"></span>
+//             <i className="fa-regular fa-heart"></i>
+//           </NavLink>
+//           <button className="menu-icon" type="button" data-bs-toggle="offcanvas" data-bs-target="#shoppingCart" aria-controls="show">
+//             <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme"></span>
+//             {/* <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme">{totalQuantity}</span> */}
+//             <i className="fa-regular fa-bag-shopping"></i>
+//           </button>
+//           <button onClick={toggleMenu} className="d-xl-none menu-icon btn-menu-icon">
+//             <i className="fa-regular fa-bars"></i>
+//           </button>
+//         </div> */}
+
+
