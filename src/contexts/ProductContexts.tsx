f6e614ff0c0af3073e997dba7322fea7a7d81eba
Zahra Parhizkari <zarapar64@gmail.com>
2022-12-25 21:53:26 +0100

    changesadded to work with databas

--- src/contexts/ProductContexts.tsx
+++ src/contexts/ProductContexts.tsx
@@ -40,7 +40,7 @@ export const useProductContext = () => { return useContext (ProductContext )}
 	const getProduct = async (articleNumber?: string ) => {
 		if (articleNumber !== undefined) {
 			// const res = await fetch(baseUrl + `/${articleNumber}`)
-			const res = await fetch(`${baseUrl}/details/${articleNumber}`)
+			const res = await fetch(`${baseUrl}/product/details/${articleNumber}`)
 			setProduct (await res.json ())
 		}
 		
